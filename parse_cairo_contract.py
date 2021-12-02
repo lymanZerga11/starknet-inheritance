c83342fef619c4a0d4c2431503205719f75c091a
tonysprocket <andrew_noel@live.com>
2021-12-02 12:31:40 -0700

    add parse lang function

--- parse_cairo_contract.py
+++ parse_cairo_contract.py
@@ -3,7 +3,7 @@ Parse a .cairo contract into the following data structure:
 
 {
     inherits: List[str]
-    lang: str
+    lang: List[str]
     builtins: List[str]
     imports: List[Dict{
         package_name: List[str] <- imports
@@ -38,6 +38,9 @@ def parse_cairo_contract(contract_path):
         contract_as_string = contract.read()
         print(contract_as_string)
 
+        # final data structure
+        dict_of_contract = dict()
+
         # compile important keywords
         dict_of_keywords = dict(
             {
@@ -61,6 +64,41 @@ def parse_cairo_contract(contract_path):
             ]
 
         print(dict_of_matches)
+
+        dict_of_contract["lang"] = parse_lang(
+            contract_as_string, dict_of_matches["lang"]
+        )
+
+        print(dict_of_contract)
+
+
+def parse_lang(contract: str, lang_match: re.Match):
+    lang_list = list()
+
+    newline = re.compile("\n")
+
+    for occurance in lang_match:
+        string_starting_with_lang = contract[occurance["start"] :]
+        match = newline.search(string_starting_with_lang)
+
+        lang_block = string_starting_with_lang[: match.start()]
+
+        lang_types = list(["starknet"])
+        compiled_langs = compile_list_of_strings(lang_types)
+
+        for lang in compiled_langs:
+            if lang.search(lang_block):
+                lang_list.append(lang.pattern)
+
+    return lang_list
+
+
+def compile_list_of_strings(list_of_strings: list()) -> list():
+    compiled_strings = list()
+    for string in list_of_strings:
+        compiled_strings.append(re.compile(string))
+
+    return compiled_strings
 
 
 parse_cairo_contract("A.cairo")
